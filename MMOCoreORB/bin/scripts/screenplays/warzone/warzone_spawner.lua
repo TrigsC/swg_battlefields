cd9a967e2841f5d05c14db973cb6a5a3870c749b
TrigsC <trigsc@gmail.com>
2021-08-29 20:29:21 -0700

    Imp starting area 4

--- MMOCoreORB/bin/scripts/screenplays/warzone/warzone_spawner.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/warzone_spawner.lua
@@ -58,5 +58,5 @@ function WarZoneSpawner:spawnSceneObjects()
     spawnSceneObject("naboo", "object/static/structure/corellia/corl_imprv_wall_4x32_s03.iff", 3571.38, -209.32, 6985.26, 9995373, math.rad(150) )
     spawnSceneObject("naboo", "object/static/structure/corellia/corl_imprv_wall_4x32_s03.iff", 3598.9, -209.34, 7001.14, 9995372, math.rad(150) )
     spawnSceneObject("naboo", "object/static/structure/corellia/corl_imprv_column_s03.iff", 3613.18, -210, 7009.50, 9995374, math.rad(150) )
-    spawnSceneObject("naboo", "object/static/structure/corellia/corl_imprv_wall_4x32_s03.iff", 3621.66, -209.07, 6995.39, 9995375, math.rad(230) )
+    spawnSceneObject("naboo", "object/static/structure/corellia/corl_imprv_wall_4x32_s03.iff", 3622.7, -209.57, 6996.46, 9995375, math.rad(239) )
 end
