3e471b276261a92a9f746f64ce70cb4c216d0033
TrigsC <trigsc@gmail.com>
2021-09-02 11:39:38 -0700

    try linkdead

--- MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -1414,7 +1414,8 @@ void PlayerObjectImplementation::notifyOnline() {
 	}
 
 	playerCreature->schedulePersonalEnemyFlagTasks();
-
+	if (player->isLinkDead())
+		info("*************ISLINKDEADTRUE************", true);
 	if (playerCreature->isImperial())
 		playerCreature->switchZone("naboo", 3600, -208, 6980, 0);
 }
