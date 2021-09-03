53a173507df1a3de621d725b0b724b4c178c24d9
TrigsC <trigsc@gmail.com>
2021-09-03 11:18:42 -0700

    convos 4

--- MMOCoreORB/bin/scripts/mobile/custom_content/tp/tp_imperial.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/tp/tp_imperial.lua
@@ -26,8 +26,7 @@ tp_imperial = Creature:new {
 	optionsBitmask = INVULNERABLE + CONVERSABLE,
 	diet = HERBIVORE,
 
-	--templates = {"object/mobile/4lom.iff"},
-	templates = {"object/mobile/dressed_fs_village_whip.iff"},
+	templates = {"object/mobile/4lom.iff"},
 	lootGroups = {},
 	weapons = {},
 	conversationTemplate = "tp_imperialConversationTemplate",
--- MMOCoreORB/bin/scripts/mobile/custom_content/tp/tp_rebel.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/tp/tp_rebel.lua
@@ -26,8 +26,7 @@ tp_rebel = Creature:new {
 	optionsBitmask = INVULNERABLE + CONVERSABLE,
 	diet = HERBIVORE,
 
-	--templates = {"object/mobile/3po_protocol_droid_red.iff"},
-	templates = {"object/mobile/dressed_fs_village_whip.iff"},
+	templates = {"object/mobile/3po_protocol_droid_red.iff"},
 	lootGroups = {},
 	weapons = {},
 	conversationTemplate = "tp_rebelConversationTemplate",
