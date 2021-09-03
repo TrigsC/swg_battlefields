eee9406993dd9a896ab19c3ac52a85d8a5dbab29
TrigsC <trigsc@gmail.com>
2021-09-03 15:13:32 -0700

    convos 5

--- MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
@@ -1,4 +1,7 @@
 --New Content Mob Template Files
 
 --Child Folders
-includeFile("custom_content/tp/serverobjects.lua")
+--includeFile("custom_content/tp/serverobjects.lua")
+
+includeFile("custom_content/tp_rebel.lua")
+includeFile("custom_content/tp_imperial.lua")
