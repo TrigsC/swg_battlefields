694b2829101968cab91875e06166cfc1520f0ddd
TrigsC <trigsc@gmail.com>
2021-09-06 20:26:15 -0700

    warzone villagesui check

--- MMOCoreORB/bin/scripts/screenplays/village/village_gm_sui.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/village_gm_sui.lua
@@ -13,7 +13,7 @@ function VillageGmSui:showMainPage(pPlayer)
 	local phaseTimeLeft = self:getPhaseDuration()
 
 	local suiPrompt = " \\#pcontrast1 " .. "Current Phase:" .. " \\#pcontrast2 " .. curPhase .. " (id " .. phaseID .. ")\n" .. " \\#pcontrast1 " .. "Current Server Time:" .. " \\#pcontrast2 " .. os.date("%c") .. "\n"
-	local suiPrompt = suiPrompt .. " \\#pcontrast1 " .. "Next Phase Change: " .. " \\#pcontrast2 " .. os.date("%c", nextPhaseChange)  .. "\n \\#pcontrast1 " .. "Phase Time Left: " .. " \\#pcontrast2 " .. phaseTimeLeft
+	local suiPrompt = suiPrompt .. " \\#pcontrast1 " .. "Next Phase Change: " .. " \\#pcontrast2 " .. os.date("%c", nextPhaseChange)  .. "\n \\#pcontrast1 " .. "Phase Time Left: " .. " \\#pcontrast2 " --.. phaseTimeLeft
 
 	local pMaster = VillageJediManagerTownship:getMasterObject()
 
