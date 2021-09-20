3e0342db7fb3f9b95b5582920d834e86541c30d5
TrigsC <trigsc@gmail.com>
2021-09-20 15:12:42 -0700

    test

--- MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/frs/FrsManagerImplementation.cpp
@@ -1082,8 +1082,8 @@ int FrsManagerImplementation::calculatePvpExperienceChange(CreatureObject* attac
 	float xpAdjustment = ((float)ratingDiff / 2000.f) * 0.5f;
 	int xpChange = getBaseExperienceGain(playerGhost, opponentGhost, !isVictim);
 
-	info("******* xpchange: " + xpChange + " *******", true);
-	info("******* contribution: " + contribution + " *******", true);
+	info("******* xpchange: " + String::valueOf(xpChange) + " *******", true);
+	info("******* contribution: " + String::valueOf(contribution) + " *******", true);
 
 	//if (xpChange != 0) {
 		//xpChange = (int)((float)xpChange * contribution);
