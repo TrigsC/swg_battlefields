56f6a24d00a7d6baeafaf13761d1f7cb09fc9c81
TrigsC <trigsc@gmail.com>
2021-09-22 07:42:07 -0700

    comp fix

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_chest_plate.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_chest_plate.lua
@@ -108,10 +108,10 @@ object_tangible_wearables_armor_composite_armor_composite_chest_plate = object_t
 	kinetic = 90,
 	energy = 90,
 	electricity = 80,
-	stun = 38,
+	stun = 40,
 	blast = 90,
-	heat = 76,
-	cold = 73,
+	heat = 90,
+	cold = 90,
 	acid = 90,
 	lightSaber = 0,
 
