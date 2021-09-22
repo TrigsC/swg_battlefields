44170522e0fb044c9b24fcb15fdc6f6741ce48b1
TrigsC <trigsc@gmail.com>
2021-09-22 14:39:08 -0700

    only spawn in phase test 2

--- MMOCoreORB/bin/scripts/screenplays/warzone/warzone_spawn_table.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/warzone_spawn_table.lua
@@ -1,8 +1,41 @@
 warzoneMobileSpawns = {
 	[1] = {
-		{ "corellia", "fbase_stormtrooper_extreme", 60, -154, 28, -4175, 0, 0 },
-		{ "corellia", "fbase_stormtrooper_extreme", 60, -174, 28, -4177, 0, 0 },
-		{ "corellia", "fbase_stormtrooper_extreme", 60, -243, 28, -4153, 0, 0 },
-		{ "corellia", "fbase_stormtrooper_extreme", 60, -250, 28, -4151, 0, 0 },
+		--{ "corellia", "fbase_stormtrooper_extreme", 60, -154, 28, -4175, 0, 0 },
+		--{ "corellia", "fbase_stormtrooper_extreme", 60, -174, 28, -4177, 0, 0 },
+		--{ "corellia", "fbase_stormtrooper_extreme", 60, -243, 28, -4153, 0, 0 },
+		--{ "corellia", "fbase_stormtrooper_extreme", 60, -250, 28, -4151, 0, 0 },
+        --imp
+        { "corellia", "fbase_stormtrooper_extreme", 60, -154, 28, -4175, 0, 0 },
+        { "corellia", "fbase_stormtrooper_extreme", 60, -174, 28, -4177, 0, 0 },
+        { "corellia", "fbase_stormtrooper_extreme", 60, -243, 28, -4153, 0, 0 },
+        { "corellia", "fbase_stormtrooper_extreme", 60, -250, 28, -4151, 0, 0 },
+        --reb
+        { "corellia", "fbase_rebel_soldier_extreme", 60, -83, 28, -4805, 0, 0 },
+        { "corellia", "fbase_rebel_soldier_extreme", 60, -80, 28, -4820, 0, 0 },
+        { "corellia", "fbase_rebel_soldier_extreme", 60, -133, 28, -4700, 0, 0 },
+        { "corellia", "fbase_rebel_soldier_extreme", 60, -154, 28, -4698, 0, 0 },
+        { "corellia", "fbase_rebel_soldier_extreme", 60, -198, 28, -4733, 0, 0 },
 	},
+    [2] = {
+        --imp
+        { "naboo", "fbase_stormtrooper_extreme", 60, -5969, 6, 4303, 40, 0 },
+        { "naboo", "fbase_stormtrooper_extreme", 60, -5975, 6, 4263, 180, 0 },
+        { "naboo", "fbase_stormtrooper_rifleman_hard", 60, -5940, 6, 4249, 90, 0 },
+        { "naboo", "fbase_stormtrooper_rifleman_hard", 60, -5976, 6, 4210, 90, 0 },
+        { "naboo", "fbase_stormtrooper_rifleman_hard", 60, -5895, 6, 4151, 90, 0 },
+        { "naboo", "fbase_stormtrooper_rifleman_hard", 60, -5897, 6, 4126, 40, 0 },
+        { "naboo", "fbase_stormtrooper_rifleman_hard", 60, -5872, 6, 4227, 40, 0 },
+        { "naboo", "fbase_stormtrooper", 60, -5835, 6, 4125, 90, 0 },
+        { "naboo", "fbase_stormtrooper", 60, -5827, 6, 4151, 90, 0 },
+        { "naboo", "fbase_stormtrooper", 60, -5812, 6, 4263, 90, 0 },
+        { "naboo", "fbase_stormtrooper", 60, -5802, 6, 4296, 90, 0 },
+        --reb
+        { "naboo", "fbase_rebel_soldier", 60, -4828, 6, 4122, 290, 0 },
+        { "naboo", "fbase_rebel_soldier", 60, -4842, 6, 4101, 290, 0 },
+        { "naboo", "fbase_rebel_soldier", 60, -4829, 6, 4064, 200, 0 },
+        { "naboo", "fbase_rebel_rifleman_hard", 60, -4681, 6, 4011, 210, 0 },
+        { "naboo", "fbase_rebel_rifleman_hard", 60, -4630, 6, 4033, 210, 0 },
+        { "naboo", "fbase_rebel_soldier_extreme", 60, -4620, 6, 4085, 190, 0 },
+        { "naboo", "fbase_rebel_soldier_extreme", 60, -4596, 6, 4118, 190, 0 },
+    }
 }
