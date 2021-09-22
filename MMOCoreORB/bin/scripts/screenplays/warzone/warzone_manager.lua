7050111a233925be9be8095e3f0e9d6cd8c55b25
TrigsC <trigsc@gmail.com>
2021-09-22 14:39:55 -0700

    only spawn in phase test 2

--- MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
@@ -232,72 +232,38 @@ end
 -- Spawning functions.
 
 function WarzoneManager:spawnMobiles(currentPhase, spawnStaticMobs)
-	--if (spawnStaticMobs == true) then
-	--	local mobileTable = warzoneMobileSpawns[0]
-	--	for i = 1, #mobileTable, 1 do
-	--		local mobile = mobileTable[i]
-	--		local pMobile = spawnMobile(mobile[1], mobile[2], mobile[3], mobile[4], mobile[5], mobile[6], mobile[7], mobile[8])
-	--	end
-	--end
+	if (spawnStaticMobs == true) then
+		local mobileTable = warzoneMobileSpawns[0]
+		for i = 1, #mobileTable, 1 do
+			local mobile = mobileTable[i]
+			local pMobile = spawnMobile(mobile[1], mobile[2], mobile[3], mobile[4], mobile[5], mobile[6], mobile[7], mobile[8])
+            
+            local mobileID = SceneObject(pMobile):getObjectID()
+			writeData("warzone:npc:object:" .. i, mobileID)
+		end
+	end
 
 	--local mobileTable = warzoneMobileSpawns[currentPhase]
-    if (currentPhase == 1) then
-        --imp
-        spawnMobile("corellia", "fbase_stormtrooper_extreme", 60, -154, 28, -4175, 0, 0)
-        spawnMobile("corellia", "fbase_stormtrooper_extreme", 60, -174, 28, -4177, 0, 0)
-        spawnMobile("corellia", "fbase_stormtrooper_extreme", 60, -243, 28, -4153, 0, 0)
-        spawnMobile("corellia", "fbase_stormtrooper_extreme", 60, -250, 28, -4151, 0, 0)
-        --reb
-        spawnMobile("corellia", "fbase_rebel_soldier_extreme", 60, -83, 28, -4805, 0, 0)
-        spawnMobile("corellia", "fbase_rebel_soldier_extreme", 60, -80, 28, -4820, 0, 0)
-        spawnMobile("corellia", "fbase_rebel_soldier_extreme", 60, -133, 28, -4700, 0, 0)
-        spawnMobile("corellia", "fbase_rebel_soldier_extreme", 60, -154, 28, -4698, 0, 0)
-        spawnMobile("corellia", "fbase_rebel_soldier_extreme", 60, -198, 28, -4733, 0, 0)
-    end
-    if (currentPhase == 2) then
-        --imp
-        spawnMobile("naboo", "fbase_stormtrooper_extreme", 60, -5969, 6, 4303, 40, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_extreme", 60, -5975, 6, 4263, 180, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_rifleman_hard", 60, -5940, 6, 4249, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_rifleman_hard", 60, -5976, 6, 4210, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_rifleman_hard", 60, -5895, 6, 4151, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_rifleman_hard", 60, -5897, 6, 4126, 40, 0)
-        spawnMobile("naboo", "fbase_stormtrooper_rifleman_hard", 60, -5872, 6, 4227, 40, 0)
-        spawnMobile("naboo", "fbase_stormtrooper", 60, -5835, 6, 4125, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper", 60, -5827, 6, 4151, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper", 60, -5812, 6, 4263, 90, 0)
-        spawnMobile("naboo", "fbase_stormtrooper", 60, -5802, 6, 4296, 90, 0)
-        --reb
-        spawnMobile("naboo", "fbase_rebel_soldier", 60, -4828, 6, 4122, 290, 0)
-        spawnMobile("naboo", "fbase_rebel_soldier", 60, -4842, 6, 4101, 290, 0)
-        spawnMobile("naboo", "fbase_rebel_soldier", 60, -4829, 6, 4064, 200, 0)
-        spawnMobile("naboo", "fbase_rebel_rifleman_hard", 60, -4681, 6, 4011, 210, 0)
-        spawnMobile("naboo", "fbase_rebel_rifleman_hard", 60, -4630, 6, 4033, 210, 0)
-        spawnMobile("naboo", "fbase_rebel_soldier_extreme", 60, -4620, 6, 4085, 190, 0)
-        spawnMobile("naboo", "fbase_rebel_soldier_extreme", 60, -4596, 6, 4118, 190, 0)
-    end
-    if (currentPhase == 3) then
-    end
-    if (currentPhase == 4) then
-    end
+    
 
 
 	--for i = 1, #mobileTable, 1 do
 	--	local mobile = mobileTable[i]
 	--	local pMobile = spawnMobile(mobile[1], mobile[2], mobile[3], mobile[4], mobile[5], mobile[6], mobile[7], mobile[8])
 	--end
+    
 end
 
 -- Despawn and cleanup current phase mobiles.
 function WarzoneManager:despawnMobiles(currentPhase)
 	local mobileTable = warzoneMobileSpawns[currentPhase]
 	for i = 1, #mobileTable, 1 do
-		local objectID = readData("WarzoneManager:object:" .. i)
+		local objectID = readData("warzone:npc:object:" .. i)
 		local pMobile = getSceneObject(objectID)
 
 		if (pMobile ~= nil) then
 			SceneObject(pMobile):destroyObjectFromWorld()
-			deleteData("WarzoneManager:object:" .. i)
+			deleteData("warzone:npc:object:" .. i)
 		end
 	end
 end
