017421e7f85b2d11a34f9d69005538fae11d6f36
TrigsC <trigsc@gmail.com>
2021-09-24 15:55:14 -0700

    time change for server

--- MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
@@ -1,6 +1,6 @@
 WarzoneManager = ScreenPlay:new {
 	WARZONE_TOTAL_NUMBER_OF_PHASES = 4,
-	phaseChangeTimeOfDay = { hour = 02, min = 30 }, -- Hour of day, server military time, to change the phase. Comment out to disable
+	phaseChangeTimeOfDay = { hour = 08, min = 30 }, -- Hour of day, server military time, to change the phase. Comment out to disable
 
 	--WARZONE_PHASE_DURATION = 168 * 60 * 60 * 1000 -- 7 days
     WARZONE_PHASE_DURATION = 24 * 60 * 60 * 1000 -- 1 hour
