db89a3f62336f68389371b74fba36f44f8c82de7
TrigsC <trigsc@gmail.com>
2021-10-04 15:03:14 -0700

    remove test

--- MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
@@ -22,7 +22,7 @@ theedObjectSpawns = {
     },
     [2] = {
             { "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5272.303223, -0.211444, 4310.262207, 9995555, 0.530207, -0.000000, 0.847868, -0.000000 },
-            { "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5322.784668, 6.000000, 4305.860352, 9995555, -0.123345, -0.000001, 0.992364, -0.000001 },
+            --{ "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5322.784668, 6.000000, 4305.860352, 9995555, -0.123345, -0.000001, 0.992364, -0.000001 },
     },
     [3] = {
             --spawnSceneObject("object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000)
