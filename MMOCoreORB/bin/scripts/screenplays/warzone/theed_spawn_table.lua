e05ab1bd1696e157d27abef31b261dd5e8be5abb
TrigsC <trigsc@gmail.com>
2021-10-05 13:27:21 -0700

    spawn rework

--- MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
@@ -7,6 +7,7 @@ theedMobileSpawns = {
 	},
 	[2] = {
 		--{ "captain_sarguillo", 5313.9, 78.5, -4161.5, -72, "", "villageSarguilloPhase2ConvoTemplate" },
+        { "naboo", "fbase_rebel_soldier_extreme", 0, -5507, 6, 4388, 180, 0 }
 	},
 	[3] = {
 		--{ "dageerin", 5238.25, 78.5, -4189.05, 97, "", "villageDageerinPhase3ConvoTemplate" },
@@ -27,4 +28,16 @@ theedObjectSpawns = {
     [3] = {
             --spawnSceneObject("object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000)
     },
+}
+
+theedPhase1Kill = {
+    { "naboo", "fbase_rebel_soldier_extreme", 0, -5592, 6, 4071, 174, 0 }
+}
+
+theedPhase2_1Kill = {
+    { "naboo", "fbase_rebel_soldier_extreme", 0, -5483, 6, 4387, 180, 0 }
+}
+
+theedPhase2_2Kill = {
+    { "naboo", "light_jedi_sentinel", 0, -5495, 6, 4406, 180, 0 }
 }
