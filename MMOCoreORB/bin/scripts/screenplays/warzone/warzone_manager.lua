7383d38e91d956e25570993994b69b9a6270db99
TrigsC <trigsc@gmail.com>
2021-10-06 16:05:30 -0700

    to number

--- MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/warzone_manager.lua
@@ -154,7 +154,7 @@ function WarzoneManager:switchToNextPhase(manualSwitch)
 	WarzoneManager:despawnMobiles(currentPhase)
 
 	if (currentPhase == 2) then
-		local theedCurrentPhase = TheedManager.getCurrentPhase()
+		local theedCurrentPhase = tonumber(TheedManager.getCurrentPhase())
 		TheedManager:despawnMobiles(theedCurrentPhase)
 		TheedManager:despawnMobilesPhases(theedCurrentPhase)
 		TheedManager.despawnSceneObjects(theedCurrentPhase)
