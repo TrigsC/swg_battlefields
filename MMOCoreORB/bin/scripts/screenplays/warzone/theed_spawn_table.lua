aa8c344d8ec62f37a0a8934d16f519817037e78e
TrigsC <trigsc@gmail.com>
2021-10-06 12:43:29 -0700

    test error

--- MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/theed_spawn_table.lua
@@ -23,19 +23,25 @@ theedMobileSpawns = {
 	},
 }
 
+theedPhase1Kill = { "naboo", "fbase_rebel_soldier_extreme", 0, -5592, 6, 4071, 174, 0 }
+
+theedPhase2_1Kill = { "naboo", "fbase_rebel_soldier_extreme", 0, -5483, 6, 4387, 180, 0 }
+
+theedPhase2_2Kill = { "naboo", "light_jedi_sentinel", 0, -5495, 6, 4406, 180, 0 }
+
 theedObjectSpawns = {
     [0] = {
-            { "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000 },
+            --{ "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000 },
     },
 	[1] = {
             { "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000 },
             --rebel banner
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5638.196777, 6.000000, 4126.457520, 9995555, 0.988043, 0.000000, -0.154177, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5610.357422, 6.000000, 4126.072754, 9995555, 0.999916, 0.000000, 0.012998, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5612.975098, 6.000000, 4083.187500, 9995555, -0.697503, -0.000001, 0.716579, -0.000001 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5615.308105, 6.000000, 4066.824219, 9995555, 0.879258, 0.000000, -0.476345, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5536.790527, 6.000000, 4054.759277, 9995555, 0.710593, 0.000000, -0.703600, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5536.135254, 6.000000, 4069.640625, 9995555, 0.710593, 0.000000, -0.703601, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5638.196777, 6.000000, 4126.457520, 9995555, 0.988043, 0.000000, -0.154177, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5610.357422, 6.000000, 4126.072754, 9995555, 0.999916, 0.000000, 0.012998, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5612.975098, 6.000000, 4083.187500, 9995555, -0.697503, -0.000001, 0.716579, -0.000001 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5615.308105, 6.000000, 4066.824219, 9995555, 0.879258, 0.000000, -0.476345, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5536.790527, 6.000000, 4054.759277, 9995555, 0.710593, 0.000000, -0.703600, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5536.135254, 6.000000, 4069.640625, 9995555, 0.710593, 0.000000, -0.703601, 0.000000 },
     },
     [2] = {
             { "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5272.303223, -0.211444, 4310.262207, 9995555, 0.530207, -0.000000, 0.847868, -0.000000 },
@@ -47,36 +53,30 @@ theedObjectSpawns = {
             { "object/static/structure/general/banner_imperial_style_01.iff", -5536.790527, 6.000000, 4054.759277, 9995555, 0.710593, 0.000000, -0.703600, 0.000000 },
             { "object/static/structure/general/banner_imperial_style_01.iff", -5536.135254, 6.000000, 4069.640625, 9995555, 0.710593, 0.000000, -0.703601, 0.000000 },
             -- rebel banner
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5478.069336, 6.000000, 4176.035156, 9995555, 0.999954, 0.000000, -0.009442, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5514.041992, 6.000000, 4176.248047, 9995555, 0.999955, 0.000000, -0.009442, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5514.003418, 6.000000, 4298.117676, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5477.852539, 6.000000, 4297.980469, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5476.764648, 6.000000, 4407.940430, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5515.029785, 6.000000, 4407.707031, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5273.244629, 6.000000, 4330.605957, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
-            { "object/static/structure/general/banner_rebel_style_01.iff",  -5279.834961, 6.000000, 4316.782715, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5478.069336, 6.000000, 4176.035156, 9995555, 0.999954, 0.000000, -0.009442, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5514.041992, 6.000000, 4176.248047, 9995555, 0.999955, 0.000000, -0.009442, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5514.003418, 6.000000, 4298.117676, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5477.852539, 6.000000, 4297.980469, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5476.764648, 6.000000, 4407.940430, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5515.029785, 6.000000, 4407.707031, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5273.244629, 6.000000, 4330.605957, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5279.834961, 6.000000, 4316.782715, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
 
             --{ "object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5322.784668, 6.000000, 4305.860352, 9995555, -0.123345, -0.000001, 0.992364, -0.000001 },
     },
     [3] = {
             -- rebel banner changed to imperial
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5478.069336, 6.000000, 4176.035156, 9995555, 0.999954, 0.000000, -0.009442, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5514.041992, 6.000000, 4176.248047, 9995555, 0.999955, 0.000000, -0.009442, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5514.003418, 6.000000, 4298.117676, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5477.852539, 6.000000, 4297.980469, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5476.764648, 6.000000, 4407.940430, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5515.029785, 6.000000, 4407.707031, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5273.244629, 6.000000, 4330.605957, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
-            { "object/static/structure/general/banner_imperial_style_01.iff",  -5279.834961, 6.000000, 4316.782715, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5478.069336, 6.000000, 4176.035156, 9995555, 0.999954, 0.000000, -0.009442, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5514.041992, 6.000000, 4176.248047, 9995555, 0.999955, 0.000000, -0.009442, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5514.003418, 6.000000, 4298.117676, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5477.852539, 6.000000, 4297.980469, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5476.764648, 6.000000, 4407.940430, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5515.029785, 6.000000, 4407.707031, 9995555, 0.999862, 0.000000, -0.016639, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5273.244629, 6.000000, 4330.605957, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5279.834961, 6.000000, 4316.782715, 9995555, 0.583474, -0.000000, 0.812132, -0.000000 },
             -- rebel banner
             { "object/static/structure/general/banner_rebel_style_01.iff", -5226.270508, 6.000000, 4306.506348, 9995555, 0.839260, 0.000000, -0.543730, 0.000000 },
             { "object/static/structure/general/banner_rebel_style_01.iff", -5232.993652, 6.009964, 4292.978516, 9995555, 0.817759, 0.000000, -0.575560, 0.000000 },
             --spawnSceneObject("object/static/structure/naboo/wall_a1_naboo_theed_style_1.iff", -5537.314941, 4.508315, 4072.702393, 9995555, 0.731819, 0.000000, -0.681491, 0.000000)
     },
 }
-
-theedPhase1Kill = { "naboo", "fbase_rebel_soldier_extreme", 0, -5592, 6, 4071, 174, 0 }
-
-theedPhase2_1Kill = { "naboo", "fbase_rebel_soldier_extreme", 0, -5483, 6, 4387, 180, 0 }
-
-theedPhase2_2Kill = { "naboo", "light_jedi_sentinel", 0, -5495, 6, 4406, 180, 0 }
