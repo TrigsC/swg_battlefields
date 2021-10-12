a1b2c798dd7d4d61423c1047f86a06c851e6d0b3
TrigsC <trigsc@gmail.com>
2021-10-12 13:28:48 -0700

    banner fix

--- MMOCoreORB/bin/scripts/screenplays/warzone/wayfar_spawn_table.lua
+++ MMOCoreORB/bin/scripts/screenplays/warzone/wayfar_spawn_table.lua
@@ -1,13 +1,13 @@
 wayfarObjectSpawns = {
     -- No Point, No Point
     [0] = {
-            { "object/tangible/event_perk/banner_tatooine_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
             { "object/tangible/event_perk/banner_tatooine_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
+            { "object/tangible/event_perk/banner_tatooine_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
     },
     -- Imperial A, No Point
 	[1] = {
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
             { "object/tangible/event_perk/banner_tatooine_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
-            { "object/static/structure/general/banner_imperial_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 }, 
     },
     -- Rebel A, No Point
     [2] = {
@@ -21,28 +21,28 @@ wayfarObjectSpawns = {
     },
     -- No Point, Rebel B
     [4] = {
-            { "object/static/structure/general/banner_rebel_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
             { "object/tangible/event_perk/banner_tatooine_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
     },
     -- Imperial A, Imperial B
     [5] = {
-            { "object/static/structure/general/banner_imperial_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
             { "object/static/structure/general/banner_imperial_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
     },
     -- Rebel A, Rebel B
     [6] = {
-            { "object/static/structure/general/banner_rebel_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
             { "object/static/structure/general/banner_rebel_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
+            { "object/static/structure/general/banner_rebel_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
     },
     -- Imperial A, Rebel B
     [7] = {
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
             { "object/static/structure/general/banner_rebel_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
-            { "object/static/structure/general/banner_imperial_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 }, 
     },
     -- Rebel A, Imperial B
     [8] = {
-            { "object/static/structure/general/banner_imperial_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
             { "object/static/structure/general/banner_rebel_style_01.iff", -5212.590332, 75.000000, -6492.770020, 9995555, 0.719771, 0.000000, 0.694211, 0.000000 },
+            { "object/static/structure/general/banner_imperial_style_01.iff", -5137.978516, 75.000000, -6604.126953, 9995555, -0.664596, -0.000003, 0.747203, -0.000003 },
     },
 }
 
