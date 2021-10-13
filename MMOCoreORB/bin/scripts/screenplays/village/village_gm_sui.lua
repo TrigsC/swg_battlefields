885adfb7b544f96c687b1c4a6ea29aa4a6f209a8
TrigsC <trigsc@gmail.com>
2021-10-13 11:17:30 -0700

    gui contrast

--- MMOCoreORB/bin/scripts/screenplays/village/village_gm_sui.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/village_gm_sui.lua
@@ -14,7 +14,7 @@ function VillageGmSui:showMainPage(pPlayer)
 	local theedCurPhase = TheedManager:getCurrentPhase()
 	local wayfarCurrentPhase = WayfarManager:getCurrentPhase()
 
-	local suiPrompt = " \\#pcontrast1 " .. "Current Phase:" .. " \\#pcontrast2 " .. curPhase .. " (id " .. phaseID .. ")\n" .. " \\#pcontrast1 " .. "Current Server Time:" .. " \\#pcontrast2 " .. os.date("%c") .. " \\#pcontrast1 " .. "Theed Phase:" .. " \\#pcontrast2 " .. theedCurPhase .. "\n" .. "Wayfar Phase:" .. " \\#pcontrast2 " .. wayfarCurrentPhase .. "\n"
+	local suiPrompt = " \\#pcontrast1 " .. "Current Phase:" .. " \\#pcontrast2 " .. curPhase .. " (id " .. phaseID .. ")\n" .. " \\#pcontrast1 " .. "Current Server Time:" .. " \\#pcontrast2 " .. os.date("%c") .. " \\#pcontrast1 " .. "Theed Phase:" .. " \\#pcontrast2 " .. theedCurPhase .. "\n" .. " \\#pcontrast1 " .. "Wayfar Phase:" .. " \\#pcontrast2 " .. wayfarCurrentPhase .. "\n"
 	local suiPrompt = suiPrompt .. " \\#pcontrast1 " .. "Next Phase Change: " .. " \\#pcontrast2 " .. os.date("%c", nextPhaseChange)  .. "\n \\#pcontrast1 " .. "Phase Time Left: " .. " \\#pcontrast2 " --.. phaseTimeLeft
 
 	--local pMaster = VillageJediManagerTownship:getMasterObject()
